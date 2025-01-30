d3a694b37df46b8b44aa3bfbf36a61cea493c3fa
amirnsamimi <amirnsamimi@gmail.com>
2025-01-30 20:28:31 +0330

    update on hook

--- src/hooks/useFilter.js
+++ src/hooks/useFilter.js
@@ -1,27 +1,63 @@
 import { useLocation, useNavigate } from "react-router-dom";
 import FormType from "../constants/FormType";
+import { useEffect, useState } from "react";
 
 export const EQUAL_SIGN = "~";
 export const AND_SIGN = "+";
 export const ARRAY_SEPARATOR = "--";
 
-function parseUrl(url) {}
+function parseUrl(url) {
+  
+}
 
 function stringifyUrl(data) {}
 
 // TODO: complete this hook
 function useFilter(formData) {
-  console.log(formData);
+  const [data, setData] = useState({});
   const location = useLocation();
   const navigate = useNavigate();
 
-  const filterState = {};
+  let filterState ;
 
   const setFilterState = (s) => {};
-  function onChange(e, name, type) {}
+  function onChange(e, name, type) {
+    if(  data['seller-type'] && data['seller-type'].find(i=>i === e.target.value) ){
+      setData(prev => {
+        const updatedSellerType = prev['seller-type'];
+        
+      
+        if (updatedSellerType.includes(e.target.value)) {
+          return {
+            ...prev,
+            'seller-type': updatedSellerType.filter(i => i !== e.target.value),
+          };
+        }
+        
 
-  function onClear(name) {}
-  function onClearAll() {}
+        return prev;
+      }); }
+   else if (type === "checkbox-group" && e !== undefined) {
+      setData((prev) => ({
+        ...prev,
+        [name]: Array.isArray(prev[name]) ? [...prev[name], e.target.value] : [e.target.value],
+      }));    } else {
+      setData((prev) => ({ ...prev, [name]: e.target.value }));
+    }
+  }
+
+  useEffect(()=>{
+    const sellerT = data['seller-type'] && data['seller-type'].join('--')
+    filterState = Object.entries(data);
+    navigate(`productName~${data['productName']}+sellerType~${sellerT}+category~${data['category']}+exist~${data['exist']}`)
+  },[data])
+ 
+  function onClear(name) {
+    setData({})
+  }
+  function onClearAll() {
+    setData({})
+  }
   return { filterState, setFilterState, onChange, onClear, onClearAll };
 }
 
